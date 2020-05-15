a36830094719c9e97e6f0bd14288fc3b1b1d64ff
poonam.sharma <poonam.sharma@decimal.co.in>
2020-05-15 19:50:46 +0530

    #2 issue

---
+++ src/main/java/com/staff_management/controller/StaffManagController.java
@@ -0,0 +1,18 @@
+package com.staff_management.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StaffManagController {
+	
+	@Value("${message}")
+	private String message;
+	
+	@RequestMapping("/")
+	public String checkActiveProfile() {
+		
+		return message;
+	}
+}
