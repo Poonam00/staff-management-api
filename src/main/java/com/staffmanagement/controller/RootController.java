b3c8b9db2fb098d1100335c26fc0b4953b6b5083
poonam.sharma <poonam.sharma@decimal.co.in>
2020-05-17 21:10:21 +0530

    Scheduler

--- src/main/java/com/staffmanagement/controller/RootController.java
+++ src/main/java/com/staffmanagement/controller/RootController.java
@@ -1,4 +1,4 @@
-package com.staff_management.controller;
+package com.staffmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/staffmanagement/controller/UtilityController.java
+++ src/main/java/com/staffmanagement/controller/UtilityController.java
@@ -1,16 +1,52 @@
-package com.staff_management.controller;
+package com.staffmanagement.controller;
 
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.staffmanagement.service.ScheduleService;
+
 @RestController
+@ControllerAdvice
 public class UtilityController {
 
-	@GetMapping("/getHeader")
+	Logger log = LoggerFactory.getLogger(UtilityController.class);
+
+	@Autowired
+	ScheduleService service;
+
+	@GetMapping("/header")
 	public Map<String, String> getHeader(@RequestHeader Map<String, String> headers) {
 		return headers;
+	}
+
+	@PostMapping("/schedule")
+	public ResponseEntity<?> schedule() {
+
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		// process request in other thread
+		executor.submit(() -> service.processRequest(5));
+
+		return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<?> handleAllExceptions(Exception ex) {
+		log.error("@ExceptionHandler: ", ex);
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
 	}
 }
