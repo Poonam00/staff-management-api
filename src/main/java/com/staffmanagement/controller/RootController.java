f32d0f692f097b1c91528375e4560782484cdd92
poonam.sharma <poonam.sharma@decimal.co.in>
2020-08-21 15:46:57 +0530

    spring cloud config client

--- src/main/java/com/staffmanagement/controller/RootController.java
+++ src/main/java/com/staffmanagement/controller/RootController.java
@@ -1,6 +1,7 @@
 package com.staffmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class RootController {
 
 	@Value("${message}")
